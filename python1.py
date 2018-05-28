dca31aa6360e028bde51201f1fd7e9cc2849c48b
ariahendrawan713 <38807873+ariahendrawan713@users.noreply.github.com>
2018-05-28 13:32:41 +0700

    create file python1.py

---
+++ python1.py
@@ -0,0 +1,13 @@
+var = 10
+if ( var == 100 ) : print "Value of expression is 100"
+else (var != 100): print "Value of expression is not 100"
+print "Good bye!"
+
+a = 33
+b = 200
+if b > a
+print("b is greater than a") % you will get an error
+
+mahasiswa = ["jangan", "lupa", "ucapkan", "salam"]
+for x in mahasiswa:
+  print(x)
